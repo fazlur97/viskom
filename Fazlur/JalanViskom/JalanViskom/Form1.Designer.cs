63ee9f374a38c5aad68a05972e60addaba3dc48f
Fazlur Rahman <fazlur97@gmail.com>
2018-10-17 12:26:20 +0700

    Viskom Palur

---
+++ Fazlur/JalanViskom/JalanViskom/Form1.Designer.cs
@@ -0,0 +1,354 @@
+﻿namespace JalanViskom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.buttonImport = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButtonTitik1 = new System.Windows.Forms.RadioButton();
+            this.radioButtonTitik2 = new System.Windows.Forms.RadioButton();
+            this.radioButtonTitik3 = new System.Windows.Forms.RadioButton();
+            this.radioButtonTitik4 = new System.Windows.Forms.RadioButton();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(906, 615);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.groupBox2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.trackBar1);
+            this.tabPage1.Controls.Add(this.buttonStop);
+            this.tabPage1.Controls.Add(this.buttonPlay);
+            this.tabPage1.Controls.Add(this.buttonImport);
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(898, 589);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButtonTitik4);
+            this.groupBox2.Controls.Add(this.radioButtonTitik3);
+            this.groupBox2.Controls.Add(this.radioButtonTitik2);
+            this.groupBox2.Controls.Add(this.radioButtonTitik1);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Location = new System.Drawing.Point(655, 241);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(237, 139);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(748, 204);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "label1";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(6, 538);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(661, 45);
+            this.trackBar1.TabIndex = 6;
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(655, 172);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(237, 23);
+            this.buttonStop.TabIndex = 5;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Location = new System.Drawing.Point(655, 143);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(237, 23);
+            this.buttonPlay.TabIndex = 4;
+            this.buttonPlay.Text = "Play";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // buttonImport
+            // 
+            this.buttonImport.Location = new System.Drawing.Point(655, 114);
+            this.buttonImport.Name = "buttonImport";
+            this.buttonImport.Size = new System.Drawing.Size(237, 23);
+            this.buttonImport.TabIndex = 3;
+            this.buttonImport.Text = "Import";
+            this.buttonImport.UseVisualStyleBackColor = true;
+            this.buttonImport.Click += new System.EventHandler(this.buttonImport_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(655, 8);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(237, 100);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(93, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "label3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(93, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "label2";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(6, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(643, 526);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.CursorChanged += new System.EventHandler(this.pictureBox1_CursorChanged);
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.pictureBox4);
+            this.tabPage2.Controls.Add(this.pictureBox3);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(683, 454);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(338, 390);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Canny Edge Detection";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(74, 390);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(128, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Background substraction ";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(265, 3);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(253, 383);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 9;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(6, 3);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(253, 383);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(225, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Set titik";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButtonTitik1
+            // 
+            this.radioButtonTitik1.AutoSize = true;
+            this.radioButtonTitik1.Location = new System.Drawing.Point(8, 47);
+            this.radioButtonTitik1.Name = "radioButtonTitik1";
+            this.radioButtonTitik1.Size = new System.Drawing.Size(54, 17);
+            this.radioButtonTitik1.TabIndex = 11;
+            this.radioButtonTitik1.TabStop = true;
+            this.radioButtonTitik1.Text = "Titik 1";
+            this.radioButtonTitik1.UseVisualStyleBackColor = true;
+            this.radioButtonTitik1.Visible = false;
+            // 
+            // radioButtonTitik2
+            // 
+            this.radioButtonTitik2.AutoSize = true;
+            this.radioButtonTitik2.Location = new System.Drawing.Point(8, 68);
+            this.radioButtonTitik2.Name = "radioButtonTitik2";
+            this.radioButtonTitik2.Size = new System.Drawing.Size(54, 17);
+            this.radioButtonTitik2.TabIndex = 12;
+            this.radioButtonTitik2.TabStop = true;
+            this.radioButtonTitik2.Text = "Titik 2";
+            this.radioButtonTitik2.UseVisualStyleBackColor = true;
+            this.radioButtonTitik2.Visible = false;
+            // 
+            // radioButtonTitik3
+            // 
+            this.radioButtonTitik3.AutoSize = true;
+            this.radioButtonTitik3.Location = new System.Drawing.Point(8, 91);
+            this.radioButtonTitik3.Name = "radioButtonTitik3";
+            this.radioButtonTitik3.Size = new System.Drawing.Size(54, 17);
+            this.radioButtonTitik3.TabIndex = 13;
+            this.radioButtonTitik3.TabStop = true;
+            this.radioButtonTitik3.Text = "Titik 3";
+            this.radioButtonTitik3.UseVisualStyleBackColor = true;
+            this.radioButtonTitik3.Visible = false;
+            // 
+            // radioButtonTitik4
+            // 
+            this.radioButtonTitik4.AutoSize = true;
+            this.radioButtonTitik4.Location = new System.Drawing.Point(8, 114);
+            this.radioButtonTitik4.Name = "radioButtonTitik4";
+            this.radioButtonTitik4.Size = new System.Drawing.Size(54, 17);
+            this.radioButtonTitik4.TabIndex = 14;
+            this.radioButtonTitik4.TabStop = true;
+            this.radioButtonTitik4.Text = "Titik 4";
+            this.radioButtonTitik4.UseVisualStyleBackColor = true;
+            this.radioButtonTitik4.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1101, 639);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.Button buttonImport;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButtonTitik4;
+        private System.Windows.Forms.RadioButton radioButtonTitik3;
+        private System.Windows.Forms.RadioButton radioButtonTitik2;
+        private System.Windows.Forms.RadioButton radioButtonTitik1;
+    }
+}
+
---
+++ Fazlur/JalanViskom/JalanViskom/Form1.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+
+namespace JalanViskom
+{
+
+    public partial class Form1 : Form
+    {
+        VideoCapture videoCapture;
+        private int vidFPS;
+        private int totalFrames;
+        private bool isPlaying;
+        private Mat currentFrame;
+        private Mat processedFrame;
+        private Mat processedFrameCanny;
+        private Mat backgroundFrame;
+        private int currentFrameNo;
+        private Image<Gray, Byte> currentImg;
+        private Image<Gray, Byte> processImg;
+        private Pen redPen;
+        private PointF[] poin;
+        private bool gantiTitik=false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            poin = new PointF[4];
+            poin[0] = new PointF(245, 225);
+            poin[1] = new PointF(363, 208);
+            poin[2] = new PointF(517, 398);
+            poin[3] = new PointF(269, 445);
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonImport_Click(object sender, EventArgs e)
+        {
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    videoCapture = new VideoCapture(openFileDialog.FileName);
+                    vidFPS = Convert.ToInt32(videoCapture.GetCaptureProperty(Emgu.CV.CvEnum.CapProp.Fps));
+                    totalFrames = Convert.ToInt32(videoCapture.GetCaptureProperty(Emgu.CV.CvEnum.CapProp.FrameCount));
+                    Image<Gray, Byte> imageCV = new Image<Gray, byte>(Properties.Resources.backgroundJalan);
+                    backgroundFrame = imageCV.Mat;
+                    isPlaying = true;
+                    currentFrame = new Mat();
+                    processedFrame = new Mat();
+                    currentFrameNo = 0;
+                    trackBar1.Minimum = 0;
+                    trackBar1.Maximum = totalFrames - 1;
+                    trackBar1.Value = 0;
+                }
+            }
+        }
+
+        private async void PlayVideo()
+        {
+            if (videoCapture == null)
+            {
+                return;
+            }
+            try
+            {
+                MCvPoint2D64f prevCentroid;
+                prevCentroid.X = 0;
+                prevCentroid.Y = 0;
+                while (isPlaying && currentFrameNo < totalFrames)
+                {
+                    var currentFrame = new Mat();
+                    var processFrame = new Mat();
+
+                    videoCapture.SetCaptureProperty(Emgu.CV.CvEnum.CapProp.PosFrames, currentFrameNo);
+                    videoCapture.Read(currentFrame);
+                    pictureBox1.Image = currentFrame.Bitmap;
+                    trackBar1.Value = currentFrameNo;
+
+
+                    redPen = new Pen(Color.FromArgb(255, 255, 0, 0));
+                    redPen.Width = 1.5f;
+                    
+                    using (Graphics gr = Graphics.FromImage(currentFrame.Bitmap))
+                    {
+                        gr.DrawLines(redPen, poin);
+                    }
+
+                    //CvInvoke.AbsDiff(currentFrame.ToImage<Gray, Byte>().Mat, backgroundFrame, processedFrame);
+                    //pictureBox2.Image = processedFrame.Bitmap;
+                    //processedFrame = BirdEyeView(currentFrame);
+                    //processedFrame = processedFrame.ToImage<Gray, Byte>().ThresholdBinary(new Gray(39), new Gray(255)).Mat;
+                    //currentFrame = currentFrame.ToImage<Gray, Byte>().Mat;
+                    //pictureBox3.Image = processedFrame.Bitmap;
+
+                    // Set the pen's width.
+                    SecondPictureBoxThings(processFrame, currentFrame, backgroundFrame, prevCentroid);
+                    currentFrameNo++;
+
+                    await Task.Delay(1000 / vidFPS);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error because of :" + ex.Message);
+            }
+        }
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            if (videoCapture != null)
+            {
+                isPlaying = true;
+                PlayVideo();
+            }
+            else
+            {
+                isPlaying = false;
+            }
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            isPlaying = false;
+            trackBar1.Value = 0;
+            currentFrameNo = 0;
+        }
+
+        private async void SecondPictureBoxThings(Mat processedFrame, Mat currentFrame, Mat backgroundFrame, MCvPoint2D64f prevCentroid)
+        {
+            CvInvoke.AbsDiff(currentFrame.ToImage<Gray, Byte>().Mat, backgroundFrame, processedFrame);
+            //backgroundFrame = currentFrame.ToImage<Gray, Byte>().ThresholdBinary(new Gray(88), new Gray(147)).Mat;
+            //CvInvoke.AbsDiff(currentFrame.ToImage<Gray, Byte>().ThresholdAdaptive(new Gray(255), Emgu.CV.CvEnum.AdaptiveThresholdType.GaussianC, Emgu.CV.CvEnum.ThresholdType.Binary, 3, new Gray(0.03)).Mat, backgroundFrame, processedFrame);
+            //backgroundFrame = currentFrame.ToImage<Gray, Byte>().Mat;
+            //pictureBox2.Image = processedFrame.ToImage<Gray, Byte>().ThresholdAdaptive(new Gray(255), Emgu.CV.CvEnum.AdaptiveThresholdType.GaussianC, Emgu.CV.CvEnum.ThresholdType.Binary, 3, new Gray(0.03)).Mat.Bitmap;
+            processedFrame = BirdEyeView(processedFrame);
+            processedFrameCanny = BirdEyeView(processedFrame);
+            processedFrameCanny = processedFrame.ToImage<Gray, Byte>().Canny(30, 100).Mat;
+            processedFrame = processedFrame.ToImage<Gray, Byte>().ThresholdBinary(new Gray(39), new Gray(159)).Erode(5).Dilate(20).Mat;
+            
+            Mat labels = new Mat();
+            Mat stats = new Mat();
+            Mat centroids = new Mat();
+            MCvPoint2D64f[] centroidPoints;
+            double x, y;
+            int n;
+            n = CvInvoke.ConnectedComponentsWithStats(processedFrame, labels, stats, centroids, LineType.EightConnected, DepthType.Cv32S);
+            centroidPoints = new MCvPoint2D64f[n];
+            centroids.CopyTo(centroidPoints);
+            if (n > 1)
+            {
+                var centroid = centroidPoints[0];
+                if (currentFrameNo > 2)
+                {
+                    var speed = Math.Round(Math.Sqrt(Math.Pow((centroid.X - prevCentroid.X), 2) + Math.Pow((centroid.Y - prevCentroid.Y), 2)) * vidFPS * 0.005, 2) / 2;
+                    label2.Text = speed + "KMh";
+                    label3.Text = speed + "KMh";
+                }
+                prevCentroid = centroid;
+            }
+            else
+            {
+                label2.Text = "0 KMh";
+                label3.Text = "0 KMh";
+            }
+            //foreach (MCvPoint2D64f point in centroidPoints)
+            //{
+            // x = point.X;
+            // y = point.Y;
+            //}
+
+            pictureBox3.Image = processedFrame.Bitmap;
+            pictureBox4.Image = processedFrameCanny.Bitmap;
+        }
+
+        
+
+
+        private Mat BirdEyeView(Mat data)
+        {
+            
+            PointF[] dsts = new PointF[4];
+            dsts[0] = new PointF(0, 1);
+            dsts[1] = new PointF(data.Rows, 0);
+            dsts[2] = new PointF(data.Rows, data.Cols);
+            dsts[3] = new PointF(0, data.Cols);
+
+
+            Mat mat = CvInvoke.GetPerspectiveTransform(poin, dsts);
+            Mat output = new Mat();
+            CvInvoke.WarpPerspective(data, output, mat, new Size(data.Rows, data.Cols));
+
+            return output;
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+            MouseEventArgs me = (MouseEventArgs)e;
+            Point coordinates = me.Location;
+            label1.Text = "X = " + coordinates.X + "Y=" + coordinates.Y;
+            if (radioButtonTitik1.Checked == true)
+            {
+                poin[0].X = coordinates.X;
+                poin[0].Y = coordinates.Y;
+            }
+            if (radioButtonTitik2.Checked == true)
+            {
+                poin[1].X = coordinates.X;
+                poin[1].Y = coordinates.Y;
+            }
+            if (radioButtonTitik3.Checked == true)
+            {
+                poin[2].X = coordinates.X;
+                poin[2].Y = coordinates.Y;
+            }
+            if (radioButtonTitik4.Checked == true)
+            {
+                poin[3].X = coordinates.X;
+                poin[3].Y = coordinates.Y;
+            }
+        }
+
+        private void pictureBox1_CursorChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (gantiTitik == false)
+            {
+                gantiTitik = true;
+                radioButtonTitik1.Visible = true;
+                radioButtonTitik2.Visible = true;
+                radioButtonTitik3.Visible = true;
+                radioButtonTitik4.Visible = true;
+            }
+            else if (gantiTitik == true)
+            {
+                radioButtonTitik1.Checked = false;
+                radioButtonTitik2.Checked = false;
+                radioButtonTitik3.Checked = false;
+                radioButtonTitik4.Checked = false;
+                gantiTitik = false;
+                radioButtonTitik1.Visible = false;
+                radioButtonTitik2.Visible = false;
+                radioButtonTitik3.Visible = false;
+                radioButtonTitik4.Visible = false;
+            }
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
